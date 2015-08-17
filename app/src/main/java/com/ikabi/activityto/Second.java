c4c2b7aefcde60dd53dca002d51eb7f1de654eb3
Heinilup <379869869@qq.com>
2015-08-18 01:23:28 +0800

    getIntent()

--- app/src/main/java/com/ikabi/activityto/Second.java
+++ app/src/main/java/com/ikabi/activityto/Second.java
@@ -2,6 +2,7 @@ package com.ikabi.activityto;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,7 +14,10 @@ public class Second extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
-
+        //获取到启动这个activity的意图
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+        System.out.println(uri.toString());
     }
     public void backto1(View v){
         Intent intent = new Intent();
